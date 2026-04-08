ed53babb8a2439a4755f3a79d4f5224bd169100c
JurRter <brendo.henrique@mail.uft.edu.br>
2026-04-08 08:27:34 -0300

    Aula 28/03

---
+++ Aula - 28;03/BinTree.h
@@ -0,0 +1,34 @@
+#ifndef BINTREE_H
+#define BINTREE_H
+
+#include <stdbool.h>
+
+typedef struct _tree BinTree;
+
+typedef struct _no TNo;
+
+BinTree* BT_create();
+
+bool BT_insert(BinTree* arvore, int x);
+/**
+ *  @param x valor do index
+ *  @param arvore recebe uma arvore
+ *  @return Se deu certo
+ */
+
+TNo* Strtobintree(char *str, int* x);
+
+void BT_pre(BinTree*);
+
+void BT_in(BinTree*);
+
+void BT_pos(BinTree*);
+
+void BT_nivel(BinTree*);
+
+bool BT_is_bst(BinTree* arvore);
+
+
+
+
+#endif
