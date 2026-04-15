63af1885d50e6b053a09abe60e7526c69065f3f0
JurRter <brendo.henrique@mail.uft.edu.br>
2026-04-15 08:22:32 -0300

    a testar

---
+++ Exercicio 1/main.c
@@ -0,0 +1,35 @@
+#include "BinTree.h"
+#include <stdio.h>
+
+int main(void) {
+    BinTree* arvore = BT_create();
+
+    if (arvore == NULL) {
+        puts("Erro ao criar arvore.");
+        return 1;
+    }
+
+    int valores[] = {10, 5, 17, 3, 7, 12, 20, 7};
+    int n = (int)(sizeof(valores) / sizeof(valores[0]));
+
+    puts("Teste de insercao:");
+    for (int i = 0; i < n; i++) {
+        if (BT_insert(arvore, valores[i])) {
+            printf("Inserido: %d\n", valores[i]);
+        } else {
+            printf("Nao inserido (duplicado/erro): %d\n", valores[i]);
+        }
+    }
+
+    puts("\nTeste de validacao BST:");
+    if (BT_is_bst(arvore)) {
+        puts("Resultado: eh arvore de busca (BST).\n");
+    } else {
+        puts("Resultado: NAO eh arvore de busca (BST).\n");
+    }
+    
+    printf("arvore tem %i niveis \n", BT_altura(arvore));
+    puts("Fim dos testes.");
+
+    return 0;
+}
